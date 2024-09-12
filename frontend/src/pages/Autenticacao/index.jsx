53504ae9cbf6c511ab6a18aec268edf7af8877f3
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2024-09-12 14:05:47 -0300

    feat: login :sparkles:

--- frontend/src/pages/Autenticacao/index.jsx
+++ frontend/src/pages/Autenticacao/index.jsx
@@ -1,14 +1,37 @@
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Toaster } from "@/components/ui/sonner";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import PasswordInput from "@/components/ui/PasswordInput";
+import Login from "@/services/autenticacao/login";
+import { toast } from "sonner";
 
 function Autenticacao() {
+  const [emailLogin, setEmailLogin] = useState("");
+  const [senhaLogin, setSenhaLogin] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const response = await Login(emailLogin, senhaLogin);
+
+      if (response.error) {
+        toast.error(response.error);
+      } else {
+        localStorage.setItem("authToken", response.token);
+        toast.success("Login realizado com sucesso");
+      }
+      // eslint-disable-next-line no-unused-vars
+    } catch (error) {
+      toast.error("Erro ao realizar login");
+    }
+  };
+
   return (
     <>
-      <div className="mt-8 ml-[664px]">
-        <Tabs defaultValue="login" className="w-[545px]">
+      <div className="flex mt-8 justify-center">
+        <Tabs defaultValue="login" className="w-full max-w-[545px]">
           <TabsList className="w-full">
             <TabsTrigger value="login" className="w-1/2">
               Login
@@ -27,14 +50,23 @@ function Autenticacao() {
                     type="email-login"
                     id="email-login"
                     placeholder="Email"
+                    value={emailLogin}
+                    onChange={(e) => setEmailLogin(e.target.value)}
                   />
                 </div>
                 <div className="w-[362px] gap-2 mt-4">
                   <Label htmlFor="senha-login">Senha</Label>
-                  <PasswordInput id="senha-login" placeholder="Senha" />
+                  <PasswordInput
+                    id="senha-login"
+                    placeholder="Senha"
+                    value={senhaLogin}
+                    onChange={(e) => setSenhaLogin(e.target.value)}
+                  />
                 </div>
               </div>
-              <Button className="w-[362px] mt-8">Conecte-se</Button>
+              <Button className="w-[362px] mt-8" onClick={handleLogin}>
+                Conecte-se
+              </Button>
             </div>
           </TabsContent>
           <TabsContent value="cadastro">
@@ -68,6 +100,7 @@ function Autenticacao() {
           </TabsContent>
         </Tabs>
       </div>
+      <Toaster richColors expand={true} closeButton />
     </>
   );
 }

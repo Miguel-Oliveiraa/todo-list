5f86698a614d894ca0552d6a0a0a59acc754ef07
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2024-09-12 14:12:32 -0300

    feat: cadastro :sparkles:

--- frontend/src/pages/Autenticacao/index.jsx
+++ frontend/src/pages/Autenticacao/index.jsx
@@ -6,11 +6,16 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import PasswordInput from "@/components/ui/PasswordInput";
 import Login from "@/services/autenticacao/login";
+import Cadastro from "@/services/autenticacao/Cadastro";
 import { toast } from "sonner";
 
 function Autenticacao() {
   const [emailLogin, setEmailLogin] = useState("");
   const [senhaLogin, setSenhaLogin] = useState("");
+  const [nomeCadastro, setNomeCadastro] = useState("");
+  const [emailCadastro, setEmailCadastro] = useState("");
+  const [senhaCadastro, setSenhaCadastro] = useState("");
+  const [confirmarSenhaCadastro, setConfirmarSenhaCadastro] = useState("");
 
   const handleLogin = async () => {
     try {
@@ -25,6 +30,30 @@ function Autenticacao() {
       // eslint-disable-next-line no-unused-vars
     } catch (error) {
       toast.error("Erro ao realizar login");
+    }
+  };
+
+  const handleCadastro = async () => {
+    try {
+      if (senhaCadastro !== confirmarSenhaCadastro) {
+        toast.error("As senhas não coincidem");
+        return;
+      }
+
+      const response = await Cadastro(
+        emailCadastro,
+        nomeCadastro,
+        senhaCadastro
+      );
+
+      if (response.error) {
+        toast.error(response.error);
+      } else {
+        toast.success("Cadastro realizado com sucesso!!");
+      }
+      // eslint-disable-next-line no-unused-vars
+    } catch (error) {
+      toast.error("Erro ao realizar cadastro!");
     }
   };
 
@@ -75,15 +104,32 @@ function Autenticacao() {
               <div className="mt-12">
                 <div className="w-[362px] gap-2">
                   <Label htmlFor="nome-cadastro">Nome</Label>
-                  <Input type="text" id="nome-cadastro" placeholder="Nome" />
+                  <Input
+                    type="text"
+                    id="nome-cadastro"
+                    placeholder="Nome"
+                    value={nomeCadastro}
+                    onChange={(e) => setNomeCadastro(e.target.value)}
+                  />
                 </div>
                 <div className="w-[362px] gap-2 mt-4">
                   <Label htmlFor="email-cadastro">Email</Label>
-                  <Input type="email" id="email-cadastro" placeholder="Email" />
+                  <Input
+                    type="email"
+                    id="email-cadastro"
+                    placeholder="Email"
+                    value={emailCadastro}
+                    onChange={(e) => setEmailCadastro(e.target.value)}
+                  />
                 </div>
                 <div className="w-[362px] gap-2 mt-4">
                   <Label htmlFor="senha-cadastro">Senha</Label>
-                  <PasswordInput id="senha-login" placeholder="Senha" />
+                  <PasswordInput
+                    id="senha-login"
+                    placeholder="Senha"
+                    value={senhaCadastro}
+                    onChange={(e) => setSenhaCadastro(e.target.value)}
+                  />
                 </div>
                 <div className="w-[362px] gap-2 mt-4">
                   <Label htmlFor="repetir-senha-cadastro">
@@ -92,10 +138,14 @@ function Autenticacao() {
                   <PasswordInput
                     id="senha-login"
                     placeholder="Confirmar senha"
+                    value={confirmarSenhaCadastro}
+                    onChange={(e) => setConfirmarSenhaCadastro(e.target.value)}
                   />
                 </div>
               </div>
-              <Button className="w-[362px] mt-8">Inscreva-se</Button>
+              <Button className="w-[362px] mt-8" onClick={handleCadastro}>
+                Inscreva-se
+              </Button>
             </div>
           </TabsContent>
         </Tabs>

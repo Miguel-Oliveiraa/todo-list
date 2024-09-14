331e4f9d9d2c7fbefef5fd2dcbf1ce6decbc0290
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2024-09-14 13:16:39 -0300

    feat: logout :sparkles:

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -83,7 +83,6 @@ function HomeScreen() {
         <header className="flex justify-between">
           <p className="text-6xl font-bold">Tarefas</p>
           <div className="flex gap-8 h-fit">
-            <p className="text-2xl">membros</p>
             <p className="text-2xl">perfil</p>
             <button
               onClick={() => {

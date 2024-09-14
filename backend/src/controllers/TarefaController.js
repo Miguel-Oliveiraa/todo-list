cae7a255641e090350bb89f26308b4493fafa6a8
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2024-09-14 12:20:55 -0300

    fix: armazenando horario de brasilia :bug:

--- backend/src/controllers/TarefaController.js
+++ backend/src/controllers/TarefaController.js
@@ -19,8 +19,15 @@ module.exports = {
       return res.status(404).json({ error: "Tarefa não encontrada" });
     }
 
+    const data = new Date();
+
+    const data_horario_brasilia = new Date(data.getTime() - 3 * 60 * 60 * 1000);
+
     try {
-      await tarefa.update({ finalizada: 1 });
+      await tarefa.update({
+        finalizada: 1,
+        data_de_termino: data_horario_brasilia,
+      });
       return res.json(tarefa);
     } catch (error) {
       return res.status(400).json({ error: "Erro ao finalizar tarefa" });
